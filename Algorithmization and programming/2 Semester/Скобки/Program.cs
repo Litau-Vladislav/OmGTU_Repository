8398c07a86ff71abc9603f192b54786a80d4a426
Litau Vladislav <144594788+Litau-Vladislav@users.noreply.github.com>
2024-05-29 21:48:00 +0600

    Проверка скобок
    Проверка скобок

---
+++ Algorithmization and programming/2 Semester/Скобки/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Skobki
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<char, char> Skobki = new Dictionary<char, char>();
+            Skobki['('] = ')';
+            Skobki['['] = ']';
+            Skobki['{'] = '}';
+            Stack<char> check = new Stack<char>();
+            Console.WriteLine("Введите выражение: ");
+            string str = Console.ReadLine();
+            foreach (char s in str)
+            {
+                if (Skobki.ContainsKey(s))
+                {
+                    check.Push(s);
+                }
+                else if (Skobki.ContainsValue(s))
+                {
+                    if (check.Count == 0 || Skobki[check.Peek()] != s)
+                    {
+                        Console.WriteLine("Скобки расставленны неверно.");
+                        check.Push(' ');
+                        break;
+                    }
+                    if (check.Count != 0) check.Pop();
+                }
+            }
+            if (check.Count == 0) Console.WriteLine("Скобки расставленны верно.");
+        }
+    }
+}
